796290e199242f42dde76163a82e1e4da374ba7b
AmurTigro1 <114070446+AmurTigro1@users.noreply.github.com>
2023-10-09 15:55:35 +0800

    Add files via upload

---
+++ Content.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h2>Josephen Stern Salera</h2>
+        <div class="buttons">
+            <h4>Buttons</h4>
+            <RegularB style="flex: 1" title="Regular" type="regular" />
+            <RegularB style="flex: 1" title="Happy" type="happy" />
+            <RegularB style="flex: 1" title="Sad" type="sad" />
+            <RegularB style="flex: 1" title="Angry" type="angry" />
+        </div>
+
+        </div>
+</template>
+
+<script>
+import RegularB from './RegularB.vue';
+
+export default {
+    props: {
+        person: Object
+    },
+    components: {
+        RegularB,
+    }
+}
+
+</script>
+
+<style scoped>
+img {
+    width: 100%;
+}
+h2 {
+    border-bottom: 1px solid black;
+}
+.buttons {
+    display: flex;
+    gap: 4px;
+}
+.container {
+    max-width: 900px;
+    border: 1px solid #888;
+    padding: 12px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 5px #888;
+    margin: 16px 0;
+    height: 100vh;
+}
+</style>
---
+++ RegularB.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="type">
+        {{title}}
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: String,
+        type: String //primary, danger, warning, success
+    }
+}
+
+</script>
+
+<style scoped>
+.regular {
+    background-color: #2686e7;
+    border-color: #06386a;
+}
+.angry {
+    background-color: #e72636;
+    border-color: #6a0609;
+}
+.sad {
+    background-color: #e76326;
+    border-color: #6a1806;
+}
+.happy {
+    background-color: #11c839;
+    border-color: #045e22;
+}
+div {
+    padding: 4px;
+    color: white;
+    border-radius: 4px;
+    border: 4px solid;
+    margin: 8px 0;
+    text-align: center;
+}
+</style>
